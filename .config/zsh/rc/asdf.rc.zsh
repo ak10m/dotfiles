6cabfb010428c2d715a84a8958a3350a73cad56e
ak10m <akio.morimoto@airits.jp>
2024-04-11 18:16:25 +0900

    add asdf config

--- .config/zsh/rc/asdf.rc.zsh
+++ .config/zsh/rc/asdf.rc.zsh
@@ -3,13 +3,26 @@
 #--------------------------------------------------
 
 () {
-  # asdf install directory
-  export ASDF_DIR=${XDG_CACHE_HOME}/asdf
+  export ASDF_CONFIG_FILE=${HOME}/.config/asdf/.asdfrc
+  export ASDF_DIR=${HOME}/.asdf
+  export ASDF_DATA_DIR=${XDG_CACHE_HOME}/asdf
+
+  if [ ! -e "${ASDF_DIR}" ]; then
+    echo "command not found: asdf"
+
+    echo -n "Do you want to install asdf? (y/N): "
+    if read -q; then
+      echo
+      git clone https://github.com/asdf-vm/asdf.git ${ASDF_DIR} --branch v0.14.0
+    fi
+  fi
 
   if [ -d "${ASDF_DIR}" ]; then
     source ${ASDF_DIR}/asdf.sh
-    #source $ASDF_DIR/completions/asdf.bash
+
+    # append completions to fpath
     fpath=(${ASDF_DIR}/completions $fpath)
+    # initialise completions with ZSH's compinit
     autoload -Uz compinit && compinit
 
     # nodejs
--- .config/zsh/rc/brew.rc.zsh
+++ .config/zsh/rc/brew.rc.zsh
@@ -6,10 +6,11 @@
   local BREW_BIN=/opt/homebrew/bin/brew
 
   if [ ! -e "${BREW_BIN}" ]; then
-    echo "brew is not found."
+    echo "command not found: brew"
 
     echo -n "Do you want to install brew? (y/N): "
     if read -q; then
+      echo
       # See: https://brew.sh/
       /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
     fi
